e25f0017361afb0b183a8aac6a24449d89e37c2f
IgPoly <ipolyphoto@gmail.com>
2024-09-11 12:14:18 +0400

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from .igt_tilescalc import IGT_SimpleTilesCalc
+NODE_CLASS_MAPPINGS = {"IGT_SimpleTilesCalc": IGT_SimpleTilesCalc}
+NODE_DISPLAY_NAME_MAPPINGS = {"IGT_SimpleTilesCalc": "IGT Simple Tiles Calc"}
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
