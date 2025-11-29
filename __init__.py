b2ffef3c9f0d0137dcde3eb5d2c8e3a9560d975d
IgPoly <ipolyphoto@gmail.com>
2025-11-29 04:44:43 +0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,12 +2,14 @@ from .igt_tilescalc import IGT_SimpleTilesCalc, IGT_ImageTilesCalc
 
 NODE_CLASS_MAPPINGS = {
     "IGT_SimpleTilesCalc": IGT_SimpleTilesCalc,
-    "IGT_ImageTilesCalc": IGT_ImageTilesCalc  # Добавили новую
+    "IGT_ImageTilesCalc": IGT_ImageTilesCalc
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "IGT_SimpleTilesCalc": "IGT Simple Tiles Calc (WxH)",
-    "IGT_ImageTilesCalc": "IGT Image Tiles Calc (Image)" # Добавили новую
+    "IGT_ImageTilesCalc": "IGT Image Tiles Calc (Image)"
 }
 
-__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
+WEB_DIRECTORY = "./js" 
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS', 'WEB_DIRECTORY']
